04993ba5d0f3c93804444e9ef1406b6f739a3c4e
cefabiano <32073405+cefabiano@users.noreply.github.com>
2020-04-17 12:19:14 -0400

    Several updates to map functionality and UI

--- Testing/app/src/main/java/com/javapapers/android/geolocationfinder/MainActivity.java
+++ Testing/app/src/main/java/com/javapapers/android/geolocationfinder/MainActivity.java
@@ -30,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
 package com.javapapers.android.geolocationfinder;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
@@ -39,6 +45,10 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -48,7 +58,7 @@ import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends Activity {
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
     // The minimum time between updates in milliseconds
@@ -56,9 +66,14 @@ public class MainActivity extends Activity implements LocationListener {
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
+    protected Location curr_loc;
+    TextView txtLat, up1, up2, up3, up4;
+    Button button;
+    protected String reason;
+
     protected Context context;
-    TextView txtLat;
-    ImageView icon;
+    ImageView icon, map, coordBG,logBG;
+    int viewHeight, viewWidth;
     String lat;
     String provider;
     protected String latitude, longitude;
@@ -69,10 +84,30 @@ public class MainActivity extends Activity implements LocationListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtLat = (TextView) findViewById(R.id.textview1);
-        icon = (ImageView) findViewById(R.id.icon);
+        txtLat = findViewById(R.id.coord);
+        up1 = findViewById(R.id.updater1);
+        up2 = findViewById(R.id.updater2);
+        up3 = findViewById(R.id.updater3);
+        up4 = findViewById(R.id.updater4);
+        icon = findViewById(R.id.icon);
+        logBG = findViewById(R.id.logBG);
+        coordBG = findViewById(R.id.coordBG);
+        map = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        viewWidth = size.x;
+        viewHeight = size.y;
+
+        //map.setImageDrawable(scaleImage(map.getDrawable(), 1));
+        //ImageView iv = (ImageView) findViewById(R.id.imageView);
+        //Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.york_hill_map);
+        //Bitmap bMapScaled = Bitmap.createScaledBitmap(bMap, 2152, 1668, true);
+        //iv.setImageBitmap(bMapScaled);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 // getting GPS status
         gps_enabled = locationManager
                 .isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -81,9 +116,9 @@ public class MainActivity extends Activity implements LocationListener {
                 .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
         }
-        if (gps_enabled) {
+        /*if (gps_enabled) {
             txtLat.setText(locationManager.getAllProviders().get(0));
             locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER, 0,
@@ -93,40 +128,160 @@ public class MainActivity extends Activity implements LocationListener {
             locationManager.requestLocationUpdates(
                     LocationManager.NETWORK_PROVIDER, 0,
                     0, this);
+        }*/
+
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (logBG.getVisibility() == View.VISIBLE)
+                {
+                    logBG.setVisibility(View.INVISIBLE);
+                    up1.setVisibility(View.INVISIBLE);
+                    up2.setVisibility(View.INVISIBLE);
+                    up3.setVisibility(View.INVISIBLE);
+                    up4.setVisibility(View.INVISIBLE);
+                    coordBG.setVisibility(View.INVISIBLE);
+                    txtLat.setVisibility(View.INVISIBLE);
+                } else {
+                    logBG.setVisibility(View.VISIBLE);
+                    up1.setVisibility(View.VISIBLE);
+                    up2.setVisibility(View.VISIBLE);
+                    up3.setVisibility(View.VISIBLE);
+                    up4.setVisibility(View.VISIBLE);
+                    coordBG.setVisibility(View.VISIBLE);
+                    txtLat.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void makeUseOfNewLocation(Location loc)
+    {
+        up4.setText(up3.getText());
+        up3.setText(up2.getText());
+        up2.setText(up1.getText());
+        if (isBetterLocation(loc, curr_loc))
+        {
+            curr_loc = loc;
+
+            txtLat.setText("Longitude:" + loc.getLongitude() + ", Latitude:" + loc.getLatitude());
+
+            double posModifierX = -1.84047826, posModifierY = -1.17059355;
+
+            int y = (int) ((41.41802426 - (loc.getLatitude() + posModifierY)) / 0.000002595);
+            y = (viewHeight / 2) - y;
+
+            int x = (int) (-(-72.913862347 - (loc.getLongitude() + posModifierX)) / 0.000002753125);
+            x = (viewWidth / 2) - x;
+            //up1.setText(String.valueOf(x));
+
+            MarginLayoutParams marginParams = new MarginLayoutParams(map.getLayoutParams());
+            marginParams.setMargins(x, y, 0, 0);
+            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(marginParams);
+            map.setLayoutParams(layoutParams);
+            //41.41633619 -72.91138041   41.41516245 (3,140,345.7819 pixels from 0) -72.91272546 (4,553,487.9288 pixels from 0)
+            //-0.91272546 (57,000.8093 pixels off) 0.41516245 (31,480.1046 pixels)
+            // 84 pixels = 0.00134505 and  89 pixels = 0.00117374  10 x pixels = 0.000160125, 10 y pixels = 0.00013188089
+            //top of map image is at 41.41802426, left of image is at -72.913862347
+            //start at half of screen height and subtract the right amount of pixels
+
+            //my living room is -71.0722472 42.585756
+            up1.setText("New loc used because: " + reason);
+        }
+        else
+        {
+            up1.setText("New loc denied");
+        }
+    }
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            reason = "past was null";
+            return true;
         }
 
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > 120000;
+        boolean isSignificantlyOlder = timeDelta < -120000;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            reason = "much newer";
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            reason = "much older";
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            reason = "more accurate";
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            reason = "newer";
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            reason = "newer";
+            return true;
+        }
+        reason = "no reason";
+        return false;
     }
-    @Override
-    public void onLocationChanged(Location location) {
-        txtLat = (TextView) findViewById(R.id.textview1);
-        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
 
-        //41.4150  -72.910
-        double x = location.getLongitude() + 72.91;
-        x = 388 * ((x * -1000) / 3);
-
-        double y = location.getLatitude() - 41.414;
-        y = 580 * ((y * 1000) / 3);
-
-        icon = (ImageView) findViewById(R.id.icon);
-        MarginLayoutParams marginParams = new MarginLayoutParams(icon.getLayoutParams());
-        marginParams.setMargins((int) (x), (int) (y), 0, 0);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(marginParams);
-        icon.setLayoutParams(layoutParams);//41.41580148  -72.91171203     41.41595363  -72.91074822     41.4152638  -72.91218972
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
-    }
+    public Drawable scaleImage (Drawable image, float scaleFactor) {
 
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
-    }
+        if ((image == null) || !(image instanceof BitmapDrawable)) {
+            return image;
+        }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude","status");
+        Bitmap b = ((BitmapDrawable)image).getBitmap();
+
+        int sizeX = Math.round(image.getIntrinsicWidth() * scaleFactor);
+        int sizeY = Math.round(image.getIntrinsicHeight() * scaleFactor);
+
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, sizeX, sizeY, false);
+
+        image = new BitmapDrawable(getResources(), bitmapResized);
+
+        return image;
     }
 }
